456be3afdda44f5e8b6a1848ca272a5dec223027
yahya0211 <yahyaagung65@gmail.com>
2025-01-03 22:12:53 +0700

    ubah server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import db from "./src/lib/db";
-import indexRouter from "./src/routers";
+import db from "./lib/db";
+import indexRouter from "./routers";
 import path from "path";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
@@ -15,13 +14,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "src/uploads")));
 app.use(cors());
 
+// Routing
 app.use(indexRouter);
 
 app.get("/", (req, res) => {
   res.send("CIRCLE APP - API");
 });
 
-app.listen(+PORT, async () => {
+// Koneksi Prisma DB
+(async () => {
   await db.$connect();
-  console.log("Server is running at port " + PORT);
-});
+  console.log("Database connected");
+})();
+
+// Ekspor handler untuk Vercel
+export default app;
